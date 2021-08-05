0536972a95f2a0325511841449c4688c1c98fe44
Sanved Hambarde <sanved.hambarde07@gmail.com>
2021-08-05 11:34:45 +0530

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+dotenv.config();
+connectDB();
+
 const app = express();
-const products = require("./data/products");
-app.listen(5000, console.log("server started successfully on port 5000"));
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV;
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.listen(
+  PORT,
+  console.log("server running in " + NODE_ENV + " successfully on port " + PORT)
+);
